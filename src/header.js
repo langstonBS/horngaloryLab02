9b272de0bfc6c939a7ee90e3c5485bf6e2feedfb
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-29 07:43:49 -0700

    css fixes

--- src/header.js
+++ src/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class header extends Component {
     render() {
         return (
-            <div>
-                I am working 
+            <div className= "header">
+                <h1>Pick your favorit animal </h1> 
             </div>
         )
     }
